1818b833325250acdca00bd5218b1778b700a0cb
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-02-15 14:17:00 +0630

    Added overlay script

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ let skill = document.getElementById('skill')
 let contact = document.getElementById('contact')
 let overlay = document.querySelector('.overlay')
 let close = document.querySelector('.close')
-let div = document.createElement('div')
 
-close.onclick = ()=>{
-    overlay.style.display = "none"
-}
+let div = document.createElement('div')
+div.className = "inner"
+
+// close.onclick = ()=>{
+//     overlay.style.display = "none"
+// }
 
 
 
@@ -22,9 +24,12 @@ about.onclick = ()=>{
 }
 
 function change_dom(func){
-    overlay.style.display = "block"
+    overlay.style.display = "flex"
     div.innerHTML = func
     overlay.appendChild(div)
+    if (!window.matchMedia("(max-width: 600px)").matches) {
+        dragElement(div);
+    }
 }
 
 function project_dom(){
@@ -45,6 +50,43 @@ function about_dom(){
     return render
 }
 
+
+
+
+// Draggable
+
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+  elmnt.onmousedown = dragMouseDown;
+
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 
 
