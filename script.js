940dd3419cb5b030e58fa251eb78b61aae5fb62a
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-02-22 13:21:17 +0630

    Add project section

--- script.js
+++ script.js
@@ -11,17 +11,21 @@ div.className = "inner"
 
 
 
-
 project.onclick = ()=>{
     change_dom(project_dom())
+    slide()
 }
 
 about.onclick = ()=>{
-    change_dom(about_dom())
+    change_dom(about_dom())    
 }
 
 contact.onclick = ()=>{
   change_dom(contact_dom())
+}
+
+skill.onclick = ()=>{
+  change_dom(skill_dom())
 }
 
 function change_dom(func){
@@ -33,17 +37,114 @@ function change_dom(func){
     }
     let close = document.querySelector('.close')
     close.onclick = ()=>{
-      overlay.style.display = "none"
+      div.classList.add('hidden');
+      setTimeout(function() {
+        div.classList.remove('hidden');
+        overlay.style.display = 'none';
+      }, 200);
   }
   
 }
 
 function project_dom(){
+
     let render = `
-    <div>
-        <button class="close">Close</button>
-        MY Projects
+    <div class="contact">
+    <div class="header">
+      <button class="close">X</button>
+      <p>
+      My Projects
+      </p>
     </div>
+    <section class="slider-wrapper">
+    <button class="slide-arrow" id="slide-arrow-prev">
+      &#8249;
+    </button>
+    <button class="slide-arrow" id="slide-arrow-next">
+      &#8250;
+    </button>
+    <ul class="slides-container" id="slides-container">
+      <li class="slide">
+      <img src="./images/game.png" class="slide-img" />
+      <div class="slide-description">
+        <h2>Gamery<h4>
+        <p>This is free to play games web application. Users can download free games in this app.</p>
+        <h4>Technologies</h4>
+          <span>HTML,</span>
+          <span>CSS,</span>
+          <span>JavaScript,</span>
+          <span>React,</span>
+          <span>Redux</span>
+      <br>
+      <br>
+        <a href="https://gamery.onrender.com/" target="_blank">Live Demo</a>
+        <a href="https://github.com/Htetaungkyaw71/Gamery" target="_blank">Source Code</a>
+      </div>
+      </li>
+        <li class="slide">
+        <img src="./images/chat.png" class="slide-img" />
+        <div class="slide-description">
+          <h2>Chat Valley<h4>
+          <p>Users can chat random strangers from around the world. Users need to allow their camera and microphone in web browsers.</p>
+          <h4>Technologies</h4>
+            <span>HTML,</span>
+            <span>CSS,</span>
+            <span>JavaScript,</span>
+            <span>Web RTC,</span>
+            <span>Nodejs</span>
+        <br>
+        <br>
+          <a href="https://chat-valley-zx8a.onrender.com/" target="_blank">Live Demo</a>
+          <a href="https://github.com/Htetaungkyaw71/Chat-Valley" target="_blank">Source Code</a>
+        </div>
+      
+
+      </li>
+        <li class="slide">
+          <img src="./images/ask.png" class="slide-img" />
+          <div class="slide-description">
+            <h2>Ask Me<h4>
+            <p>
+            Question and Answer application. Users can create account and ask questions.</p>
+            <h4>Technologies</h4>
+              <span>HTML,</span>
+              <span>CSS,</span>
+              <span>JavaScript,</span>
+              <span>React,</span>
+              <span>Redux,</span>
+              <span>Firebase</span>
+          <br>
+          <br>
+            <a href="https://askme-sigma.vercel.app/" target="_blank" >Live Demo</a>
+            <a href="https://github.com/Htetaungkyaw71/askme" target="_blank">Source Code</a>
+          </div>
+        </li>
+      <li class="slide">
+        <img src="./images/s.png" class="slide-img" />
+        <div class="slide-description">
+          <h2>Shoppe<h4>
+          <p>
+          A simple and small ecommerce application. Users can view products and can add products to cart and can checkout.</p>
+          <h4>Technologies</h4>
+            <span>HTML,</span>
+            <span>CSS,</span>
+            <span>JavaScript,</span>
+            <span>React,</span>
+            <span>Redux</span>
+        <br>
+        <br>
+          <a href="https://shoppe-eltm.onrender.com/" target="_blank" >Live Demo</a>
+          <a href="https://github.com/Htetaungkyaw71/Ecommerce" target="_blank">Source Code</a>
+        </div>
+    </li>
+    </ul>
+  </section>
+   </div>
+
+
+
+
+
     `
     return render
 }
@@ -75,6 +176,43 @@ function contact_dom(){
           <textarea name="message" Placeholder="Message" class="message" cols="58" rows="10" required></textarea>
           <input type="submit" value="Send" class="send">
       </form>
+  </div>
+
+ 
+</div>
+  `
+  return render
+}
+
+function skill_dom(){
+  let render = `
+  <div class="skill">
+  <div class="header">
+      <button class="close">X</button>
+      <p>
+          My Skills
+      </p>
+  </div>
+  <div class="skill-main">
+  <div>
+      <div class="tech">
+            <img src="./images/html.png" alt="">
+            <img src="./images/css.png" alt="">
+            <img src="./images/js.png" alt="">
+        </div>
+          <div class="tech">
+            <img src="./images/python.png" alt="">
+            <img src="./images/ruby.png" alt="">
+            <img src="./images/rails.png" alt="">
+          </div>
+          <div class="tech">
+            <img src="./images/physics.png" alt="">
+              <img src="./images/django.png" alt="">
+              <img src="./images/redux.png" alt="">
+          </div>
+      
+  </div>
+    
   </div>
 
  
@@ -147,8 +285,20 @@ function dragElement(elmnt) {
   }
 }
 
-
-
-
-
+function slide(){
+  const slidesContainer = document.getElementById("slides-container");
+  const slide = document.querySelector(".slide");
+  const prevButton = document.getElementById("slide-arrow-prev");
+  const nextButton = document.getElementById("slide-arrow-next");
+  
+  nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+  });
+  
+  prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+  });
+}
 
